5c0718c74d3db8d4bdc1d534276ddbcea2eaa620
aoponcedeleon <aoponcedeleon@up.edu.ph>
2020-11-07 22:14:56 +0800

    Finished Sidebar layout

--- src/features/Sidebar.js
+++ src/features/Sidebar.js
@@ -4,6 +4,12 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import SidebarChannel from './SidebarChannel';
 import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import CallIcon from '@material-ui/icons/Call';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import { Avatar } from '@material-ui/core';
+import MicIcon from '@material-ui/icons/Mic';
+import HeadsetIcon from '@material-ui/icons/Headset';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 function Sidebar() {
     return (
@@ -34,6 +40,30 @@ function Sidebar() {
                     className="sidebar__voiceIcon"
                     fontSize="large"
                 />
+
+                <div className="sidebar__voiceInfo">
+                    <h3>Voice Connected</h3>
+                    <p>Stream</p>
+                </div>
+
+                <div className="sidebar__voiceIcons">
+                    <InfoOutlinedIcon />
+                    <CallIcon />
+                </div>
+            </div>
+
+            <div className="sidebar__profile">
+                <Avatar src="https://avatars2.githubusercontent.com/u/28593720?s=460&u=99b29f4ebeb377b6275c7847b3dd69de3fdd1137&v=4" /> 
+                <div className="sidebar__profileInfo">
+                    <h3>@imallenponce</h3>
+                    <p>#thisisMyId</p>
+                </div>
+
+                <div className="sidebar__profileIcons">
+                    <MicIcon />
+                    <HeadsetIcon />
+                    <SettingsIcon />
+                </div>
             </div>
         </div>
     )
