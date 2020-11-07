4fcd8e2c6ecf3b56c79cf924aefde40b587ac0bf
aoponcedeleon <aoponcedeleon@up.edu.ph>
2020-11-07 23:54:53 +0800

    Added chat layouts

--- src/features/ChatHeader.js
+++ src/features/ChatHeader.js
@@ -3,11 +3,13 @@ import './ChatHeader.css'
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
 import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
+import SendRoundedIcon from '@material-ui/icons/SendRounded';
+import HelpRoundedIcon from '@material-ui/icons/HelpRounded';
 
 function ChatHeader() {
     return (
         <div className="chatHeader">
-            <h3>I am the Header</h3>
             <div className="chatHeader__left">
                 <h3>
                     <span className="chatHeader__hash">
@@ -22,6 +24,14 @@ function ChatHeader() {
                 <NotificationsIcon />
                 <EditLocationRoundedIcon />
                 <PeopleAltRoundedIcon />
+
+                <div className="chatHeader__search">
+                    <input placeholder="Search" />
+                    <SearchRoundedIcon />
+                </div>
+
+                <SendRoundedIcon />
+                <HelpRoundedIcon />
             </div>
         </div>
     )
---
+++ src/features/Message.js
@@ -0,0 +1,18 @@
+import { Avatar } from '@material-ui/core'
+import React from 'react'
+import './Message.css'
+
+function Message() {
+    return (
+        <div className='message'>
+            <Avatar />
+            <div className="message__info">
+                <h4>imallenponce <span className='message__timestamp'>this is a timestamp</span></h4>
+                <p>This is a message</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Message
